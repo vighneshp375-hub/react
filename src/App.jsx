8c1d45bf91ebf327fcc702cbd942d3d3f6d35fc8
vighneshp375-hub <vighneshp375@gmail.com>
2026-05-22 14:46:50 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Navbar1 from './Navbar1'
+import Button1 from './Button1'
+
+function App() {
+   return (
+    <div>
+      <Navbar1></Navbar1>
+      <Button1></Button1>
+    </div>
+  );
+}
+
+export default App;
